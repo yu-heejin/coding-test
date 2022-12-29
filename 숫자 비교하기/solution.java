5c8f1cf5678949b1d12303a84d106ced08f643bc
Yu Hee Jin <96467030+yu-heejin@users.noreply.github.com>
2022-12-30 00:59:25 +0900

    Update solution.java

--- 숫자 비교하기/solution.java
+++ 숫자 비교하기/solution.java
@@ -1,6 +1,6 @@
 class Solution {
-    public final int SAME_NUMBER = 1;
-    public final int NOT_SAME_NUMBER = -1;
+    private final int SAME_NUMBER = 1;
+    private final int NOT_SAME_NUMBER = -1;
     
     public int solution(int num1, int num2) {
         if (num1 == num2) {
