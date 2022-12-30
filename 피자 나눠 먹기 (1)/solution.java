2dac3dcae8471e17bb623795cb2c69952107e271
Yu Hee Jin <96467030+yu-heejin@users.noreply.github.com>
2022-12-31 02:03:40 +0900

    Create solution.java

---
+++ 피자 나눠 먹기 (1)/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    private final int PIZZA_PIECE = 7;
+    public int solution(int n) {
+        int answer = n / PIZZA_PIECE;
+        
+        if (n % PIZZA_PIECE > 0) {
+            answer++;
+        }
+        
+        return answer;
+    }
+}
