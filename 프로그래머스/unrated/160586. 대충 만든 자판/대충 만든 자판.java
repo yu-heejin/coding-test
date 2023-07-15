d343f42dd4b3f9cd4e78211e473c1e6a7c4dcbd0
yu-heejin <96467030+yu-heejin@users.noreply.github.com>
2023-07-15 13:25:21 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.25 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    private Map<Character, Integer> keyCount;
+    
+    public int[] solution(String[] keymap, String[] targets) {
+        keyCount = new HashMap<>();
+        
+        // 키맵에서 각 알파벳을 누를 때의 최소값을 저장한다.
+        for (int i = 0; i < keymap.length; i++) {
+            for (int j = 0; j < keymap[i].length(); j++) {
+                char key = keymap[i].charAt(j);
+                int count = keyCount.getOrDefault(key, 101);
+                
+                if ((j + 1) < count) {
+                    keyCount.put(key, j + 1);
+                }
+            }
+        }
+        
+        // target 구하기
+        int[] result = new int[targets.length];
+        
+        Arrays.fill(result, 0);
+        
+        for (int i = 0; i < targets.length; i++) {
+            for (int j = 0; j < targets[i].length(); j++) {
+                char key = targets[i].charAt(j);
+                
+                if (keyCount.containsKey(key)) {
+                    result[i] += keyCount.get(key);
+                } else {
+                    result[i] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
