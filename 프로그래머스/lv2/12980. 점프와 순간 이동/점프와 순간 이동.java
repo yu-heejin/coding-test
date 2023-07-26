e0809c3a8213c4f4b509999e4d45bbac410c6ef0
yu-heejin <96467030+yu-heejin@users.noreply.github.com>
2023-07-26 17:07:26 +0900

    Update 점프와 순간 이동.java

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class Solution {
     /*
+        2 나누기 2는 0이 아니라 1이다!!!!!
         n = 5
         1. 2.5 -> 4 (1)
         2. 2
-        3. 0
+        3. 1 -> 0 (1)
         
         n = 6
         1. 3
         2. 1.5 -> 2 (1)
-        3. 0
+        3. 1 -> 0 (1)
     */
     public int solution(int n) {
         int usage = 0;
