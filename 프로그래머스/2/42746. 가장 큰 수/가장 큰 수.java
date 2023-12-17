cbfaac8983c5699a6c210b73aa01afc8f8bb407d
yu-heejin <96467030+yu-heejin@users.noreply.github.com>
2023-12-17 23:24:17 +0900

    [level 2] Title: 가장 큰 수, Time: 1.57 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] numberToString = new String[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            numberToString[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(numberToString, (o1, o2) -> {
+            long result1 = Long.parseLong(o1 + o2);
+            long result2 = Long.parseLong(o2 + o1);
+            
+            if (result1 > result2) {
+                return -1;    
+            } else if (result2 > result1) {
+                return 1;
+            }
+            
+            return 0;  // 이 부분 없으면 런타임 에러
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (String n : numberToString) {
+            sb.append(n);
+        }
+        
+        if (sb.toString().charAt(0) == '0') return "0";
+        
+        return sb.toString();
+    }
+}
