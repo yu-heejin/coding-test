bd402a7a68fc5dc1e129a69fb1bb1e1032faf1a0
yu-heejin <96467030+yu-heejin@users.noreply.github.com>
2024-02-10 20:23:00 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -5,6 +5,7 @@ WITH join_2021_user AS (
     WHERE YEAR(joined) = '2021'
 )
 
+# 사용자의 수를 카운팅하기 때문에 DISTINCT를 붙여야 정확한 결과가 나온다. (구매 횟수 X)
 SELECT YEAR(O.sales_date) AS year
     , MONTH(O.sales_date) AS month
     , COUNT(DISTINCT O.user_id) AS puchased_users
