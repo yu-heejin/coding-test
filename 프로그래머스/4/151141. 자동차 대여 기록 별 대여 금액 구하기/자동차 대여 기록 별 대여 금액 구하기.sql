c7627239916d2334ca8ee4de13d1e944d3d643fb
yu-heejin <yuheejin0914@gmail.com>
2024-02-29 16:03:10 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,28 +1,26 @@
-# IN 절에는 WITH절을 사용할 수 없다.
-# 하나의 히스토리에 하나의 할인 정책이 붙어야함
-WITH truck_car AS (
-    SELECT C.car_id
-        , H.history_id
+# 타입이 트럭인 자동차들의 대여 기간
+WITH car_date_and_discount AS (
+    SELECT H.history_id
+        , H.car_id
         , C.car_type
         , C.daily_fee
-        , DATEDIFF(H.end_date, H.start_date) + 1 AS duration
+        , DATEDIFF(end_date, start_date) + 1 AS duration
+        , CASE 
+            WHEN DATEDIFF(end_date, start_date) + 1 >= 7 AND DATEDIFF(end_date, start_date) + 1 < 30 THEN '7일 이상'
+            WHEN DATEDIFF(end_date, start_date) + 1 >= 30 AND DATEDIFF(end_date, start_date) + 1 < 90 THEN '30일 이상'
+            WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
+            ELSE null
+        END AS period
     FROM car_rental_company_rental_history H
         JOIN car_rental_company_car C ON H.car_id = C.car_id
     WHERE C.car_type = '트럭'
 )
 
-SELECT T.history_id
-    , ROUND(T.daily_fee * (1 - IFNULL(P.discount_rate, 0) / 100) * T.duration) AS fee
-FROM truck_car T
-    LEFT JOIN car_rental_company_discount_plan P ON T.car_type = P.car_type
-        AND P.duration_type = 
-            CASE 
-                WHEN T.duration >= 90 THEN '90일 이상'
-                WHEN T.duration >= 30 THEN '30일 이상'
-                WHEN T.duration >= 7 THEN '7일 이상'
-                ELSE null
-            END
+SELECT D.history_id
+    , ROUND((D.daily_fee - (D.daily_fee * (IFNULL(P.discount_rate, 0) / 100))) * D.duration) AS fee
+FROM car_date_and_discount D
+    LEFT JOIN car_rental_company_discount_plan P ON D.car_type = P.car_type
+        AND D.period = P.duration_type
 ORDER BY fee DESC
-    , T.history_id DESC
-
+    , history_id DESC
 ;
