9036a248aea427b0cddcc8deed92531708bba8b5
yu-heejin <yuheejin0914@gmail.com>
2024-03-02 15:41:27 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,29 +1,34 @@
-# 11월 대여 가능 조건
-# 1. 대여 시작 날짜가 11월 1일 이전
-# 2. 대여 종료 날짜가 11월 1일 이전
-# 3. 대여 시작 날짜가 없는 경우
-# 기록은 여러개이며, 이 중 11월 이후 기록이 있는 아이디를 제거
-
-SELECT C.car_id
-    , C.car_type
-    , ROUND(C.daily_fee * (1 - D.discount_rate / 100) * 30) AS fee
-FROM car_rental_company_car C
-    JOIN (
-        SELECT duration_type
-            , car_type
-            , discount_rate
-        FROM car_rental_company_discount_plan
-        WHERE duration_type = '30일 이상'
-            AND (car_type = '세단' OR car_type = 'SUV')
-    ) D ON D.car_type = C.car_type
-WHERE C.car_id NOT IN (
+# 자동차 종류가 세단, SUV인 자동차 중 11월에 대여 가능한 자동차
+# 11월에 대여 가능하려면 11월 1일 ~ 30일 사이에 기록이 없어야한다
+WITH sedan_suv_car AS (
+    SELECT DISTINCT C.car_id
+        , C.car_type
+        , C.daily_fee
+        , C.options
+    FROM car_rental_company_car C
+        LEFT JOIN car_rental_company_rental_history H ON C.car_id = H.car_id
+    WHERE (C.car_type = '세단' OR C.car_type = 'SUV')
+        AND C.car_id NOT IN (
+            SELECT car_id
+            FROM car_rental_company_rental_history
+            WHERE start_date >= '2022-11-01'
+                OR end_date >= '2022-11-01'
+        )
+),
+# 30일간의 대여 금액
+month_rental_fee AS (
     SELECT C.car_id
-    FROM car_rental_company_rental_history H
-        RIGHT OUTER JOIN car_rental_company_car C ON H.car_id = C.car_id
-    WHERE H.start_date >= '2022-11-01' OR H.end_date >= '2022-11-01'
+        , C.car_type
+        , ROUND(C.daily_fee * (1 - P.discount_rate / 100)) * 30 AS fee
+    FROM sedan_suv_car C
+        JOIN car_rental_company_discount_plan P ON C.car_type = P.car_type
+            AND P.duration_type = '30일 이상'
 )
-    AND (C.car_type = '세단' OR C.car_type = 'SUV')
-    AND C.daily_fee * (1 - D.discount_rate / 100) * 30 BETWEEN 500000 AND 2000001
+
+SELECT *
+FROM month_rental_fee
+WHERE fee >= 500000
+    AND fee < 2000000
 ORDER BY fee DESC
     , C.car_type
     , C.car_id DESC
