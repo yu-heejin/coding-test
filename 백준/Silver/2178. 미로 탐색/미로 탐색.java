0baa6a7207f7136425fc1b989c933f0d85d88a89
yu-heejin <yuheejin0914@gmail.com>
2024-04-03 20:26:56 +0900

    [Silver I] Title: 미로 탐색, Time: 188 ms, Memory: 16660 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,11 +1,10 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     
-    // 상하좌우로 움직이기 위한 상수
-    static final int[] MOVE_X = { -1, 1, 0, 0 };
-    static final int[] MOVE_Y = { 0, 0, -1, 1 };
+    static final int[] MOVE_X = {-1, 1, 0, 0};
+    static final int[] MOVE_Y = {0, 0, -1, 1};
     static int n, m;
     static int[][] board;
     
@@ -13,7 +12,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] input = br.readLine().split(" ");
-        
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
         
@@ -26,32 +24,35 @@ public class Main {
             }
         }
         
-        System.out.println(bfs());
+        bfs(0, 0);
     }
     
-    private static int bfs() {
+    private static void bfs(int x, int y) {
         Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
         
-        // 출발 지점 insert
-        q.offer(new int[] {0, 0});
+        q.offer(new int[] {x, y, 1});     // 현재 위치와 현재 위치일 때의 이동한 칸의 수
+        visited[x][y] = true;
         
         while (!q.isEmpty()) {
             int[] curr = q.poll();
             
-            // 상하좌우 탐색
+            if (curr[0] == n - 1 && curr[1] == m - 1) {
+                System.out.println(curr[2]);
+                break;
+            }
+            
             for (int i = 0; i < 4; i++) {
                 int nx = curr[0] + MOVE_X[i];
                 int ny = curr[1] + MOVE_Y[i];
                 
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                 
-                if (board[nx][ny] == 1) {
-                    q.offer(new int[] {nx, ny});
-                    board[nx][ny] = board[curr[0]][curr[1]] + 1;
+                if (!visited[nx][ny] && board[nx][ny] == 1) {
+                    q.offer(new int[] {nx, ny, curr[2] + 1});
+                    visited[nx][ny] = true;
                 }
             }
         }
-        
-        return board[n - 1][m - 1];
     }
 }
