445df7b1b1a100e5e482bb73473eabd6040a7344
yu-heejin <yuheejin0914@gmail.com>
2024-04-04 23:32:25 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18564 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,52 +3,51 @@ import java.util.*;
 
 public class Main {
     
-    static boolean[] visited = new boolean[100001];
+    static int[] board = new int[100001];
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] input = br.readLine().split(" ");
-        
         int n = Integer.parseInt(input[0]);
         int k = Integer.parseInt(input[1]);
         
-        if (n == k) System.out.println(0);
-        else bfs(n, k);
+        Arrays.fill(board, -1);
+        
+        bfs(n, k);
     }
     
-    private static void bfs(int start, int target) {
-        Queue<int[]> q = new LinkedList<>();
-        // 각 방향으로 움직였을 때의 최소값
-        q.add(new int[] {start, 0});
-        int max = Integer.MIN_VALUE;
+    private static void bfs(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(n);     // 현재 위치
+        board[n] = 0;   // 그 때의 시간
         
         while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            visited[curr[0]] = true;
+            int curr = q.poll();
             
-            // 현재 위치에서 이동 시 동생의 위치인 경우
-            if (curr[0] - 1 == target || curr[0] + 1 == target || curr[0] * 2 == target) {
-                System.out.println(curr[1] + 1);
-                break;
+            // 도착한 경우
+            if (curr == k) {
+                System.out.println(board[curr]);
+                return;
             }
             
-            // x - 1로 이동했을 때의 좌표와 그 때의 시간
-            if (curr[0] - 1 >= 0 && !visited[curr[0] - 1]) {
-                visited[curr[0] - 1] = true;
-                q.add(new int[] {curr[0] - 1, curr[1] + 1});
+            // 범위를 벗어나지 않았고 방문하지 않은 경우
+            if (curr - 1 >= 0 && board[curr - 1] == -1) {
+                q.add(curr - 1);
+                board[curr - 1] = board[curr] + 1;
             }
             
-            // x + 1로 이동했을 때의 좌표와 그 때의 시간
-            if (curr[0] + 1 <= 100000 && !visited[curr[0] + 1]) {
-                visited[curr[0] + 1] = true;
-                q.add(new int[] {curr[0] + 1, curr[1] + 1});
+            // 범위를 벗어나지 않았고 방문하지 않은 경우
+            if (curr + 1 <= 100000 && board[curr + 1] == -1) {
+                q.add(curr + 1);
+                board[curr + 1] = board[curr] + 1;
             }
             
-            // 2 * x로 이동했을 때의 좌표와 그 때의 시간
-            if (2 * curr[0] <= 100000 && !visited[2 * curr[0]]) {
-                visited[curr[0] * 2] = true;
-                q.add(new int[] {curr[0] * 2, curr[1] + 1});
+            // 범위를 벗어나지 않았고 방문하지 않은 경우
+            if (curr * 2 <= 100000 && board[curr * 2] == -1) {
+                q.add(curr * 2);
+                board[curr * 2] = board[curr] + 1;
             }
         }
     }
