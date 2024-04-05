218787e1b878d2e0e128db5c3e3b96eac9fdf556
yu-heejin <yuheejin0914@gmail.com>
2024-04-05 13:53:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,63 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    // 다시 풀어볼 문제...
-    final static int MAX_LENGTH = 11;
-    static Scanner sc = new Scanner(System.in);
     
-    public static int returnDPValue(int index) {
-        if (index == 1) {
-            return 1;
-        }
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        if (index == 2) {
-            return 2;
-        }
+        int t = Integer.parseInt(br.readLine());
         
-        if (index == 3) {
-            return 4;
-        }
-        
-        return 0;
-    }
-    
-    public static int[] initDPArray(int[] dp) {
-        for (int i = 0; i <= MAX_LENGTH; i++) {
-            dp[i] = returnDPValue(i);
-        }
-        
-        return dp;
-    }
-    
-    public static void initNValue(int t, int[] dp) {
         for (int i = 0; i < t; i++) {
-            int n = sc.nextInt();
-            printCalculationResult(n, dp);
+            int n = Integer.parseInt(br.readLine());
+            int[] dp = new int[n + 1];
+            
+            if (n == 1) {
+                dp[1] = 1;
+            } else if (n == 2) {
+                dp[1] = 1;
+                dp[2] = 2;
+            } else {
+                dp[1] = 1;
+                dp[2] = 2;
+                dp[3] = 4;
+            }
+            
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+            
+            System.out.println(dp[n]);
         }
-    }
-    
-    public static int[] calculationResult(int[] dp) {
-        for (int i = 4; i <= MAX_LENGTH; i++) {
-            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
-            // 패턴을 보면 해당 점화식을 도출할 수 있음
-        }
-        
-        return dp;
-    }
-    
-    public static void printCalculationResult(int n, int[] dp) {
-        System.out.println(dp[n]);
-    }
-    
-    public static void main(String[] args) {
-        int t = sc.nextInt();
-        int[] dp = new int[MAX_LENGTH + 1];
-        
-        dp = initDPArray(dp);
-        dp = calculationResult(dp);
-        
-        initNValue(t, dp);
-        
-        sc.close();
     }
 }
