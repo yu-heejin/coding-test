096cf2eef2c6c02f5643bfbad3023f7de464751d
yu-heejin <yuheejin0914@gmail.com>
2024-05-31 16:30:59 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1596 ms, Memory: 40144 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,42 +1,52 @@
-import java.util.Scanner;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
-    public static boolean[] arr = new boolean[1000001];
     
-    public static void eratosthenes() {
-        Arrays.fill(arr, true);
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        arr[1] = false;
-        
-        for(int i=2; i<=1000000; i++) {
-            for(int j=i*2; j<=1000000; j+=i) {
-                arr[j] = false;
-            }
-        }
-    }
-    
-    public static void main(String[] args) {
-        // 4보다 큰 모든 짝수는 두 홀수 소수의 합으로 나타낼 수 있다.
-        Scanner sc = new Scanner(System.in);
-        eratosthenes();
-        
-        boolean flag = false;
-        //입력은 하나 또는 그 이상의 테스트 케이스로 이루어져 있다.
-        //b-a의 값이 가장 커야한다. -> 가장 큰 수에서 감소시켜가면서 구한다.
-        do {
-            int n = sc.nextInt();
+        // 100만에 대한 소수 판별을 미리 해둔다.
+        boolean[] isPrime = new boolean[1000001];
             
-            for(int i=1; i<=n; i++) {
-                if((arr[n-i] == true) && (arr[i] == true)) {
-                    //System.out.println(n-i);
-                    System.out.println(n + " = " + i + " + " + (n-i));
-                    flag = true;
-                    break;
+        Arrays.fill(isPrime, true);
+        
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        for (int i = 2; i <= Math.sqrt(1000000); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= 1000000; j += i) {
+                    isPrime[j] = false;
                 }
             }
-        } while(sc.hasNextInt());
+        }
         
-        if(!flag) System.out.println("Goldbach's conjecture is wrong.");
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            
+            if (n == 0) break;
+            
+            // 두 수의 차가 가장 작은 수
+            int a = 2;
+            boolean isWrong = true;
+            
+            while (a <= n) {
+                int b = n - a;
+                
+                // 두 수가 모두 소수인 경우
+                if (isPrime[a] && isPrime[b]) {
+                    System.out.println(n + " = " + a + " + " + b);
+                    isWrong = false;
+                    break;
+                }
+                
+                a++;
+            }
+            
+            if (isWrong) {
+                System.out.println("Goldbach's conjecture is wrong.");
+            }
+        }
     }
 }
