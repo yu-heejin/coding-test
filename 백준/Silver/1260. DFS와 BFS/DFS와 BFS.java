b4b8dcdee6635b651cb63eaca54d06cb2b235cfe
yu-heejin <yuheejin0914@gmail.com>
2024-09-08 23:21:14 +0900

    [Silver II] Title: DFS와 BFS, Time: 272 ms, Memory: 21088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,26 +3,27 @@ import java.util.*;
 
 public class Main {
     
-    static List<List<Integer>> graph;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        int v = Integer.parseInt(input[2]);
         
-        graph = new ArrayList<>();
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int V = Integer.parseInt(input[2]);
         
-        // graph 초기화
-        for (int i = 0; i <= n; i++) {
+        visited = new boolean[N + 1];
+            
+        // 1. init graph
+        for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<Integer>());
         }
         
-        // 그래프에 노드 삽입
-        // 입력으로 주어지는 간선은 양방향이다.
-        for (int i = 0; i < m; i++) {
+        // 2. 간선 정보 저장
+        for (int i = 0; i < M; i++) {
             input = br.readLine().split(" ");
             
             int a = Integer.parseInt(input[0]);
@@ -32,65 +33,60 @@ public class Main {
             graph.get(b).add(a);
         }
         
-        // dfs
-        dfs(n, v);
+        // 3. 간선 오름차순 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+        
+        // result dfs
+        dfs(V);
+        
+        Arrays.fill(visited, false);
         
         System.out.println();
         
-        // bfs
-        bfs(n, v);
+        bfs(V);
     }
     
-    // dfs: stack을 이용한 구현
-    // 재귀 호출(함수)도 내부적으로 stack을 이용함을 기억할 것
-    private static void dfs(int n, int start) {
-        boolean[] visited = new boolean[n + 1];
-        Stack<Integer> s = new Stack<>();
+    /* BFS */
+    private static void bfs(int start) {
+        // 정점 번호가 가장 작은 것을 먼저 방문
+        Queue<Integer> q = new LinkedList<>();
         
-        // stack을 생성하고 시작 정점의 값을 스택에 넣는다.
-        s.push(start);
+        visited[start] = true;
+        q.add(start);
         
-        while (!s.isEmpty()) {
-            // pop해서 스택 최상단 노드를 꺼낸다
-            int v = s.pop();
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            System.out.print(curr + " ");
             
-            // 만약 방문한 정점이 아니라면
-            if (!visited[v]) {
-                visited[v] = true;
-                System.out.print(v + " ");     // 이미 방문한 경우 출력하지 않는다.
+            List<Integer> connections = graph.get(curr);
             
-                // 해당 노드의 인접 노드 중 미방문 노드를 전부 push
-                // 이 때 스택에 중복 push는 없도록 한다.
-                List<Integer> conn = graph.get(v);
-                Collections.sort(conn, Collections.reverseOrder());
-                for (int c : conn) {
-                    if (!visited[c]) {
-                        s.push(c);
-                    }
-                }    
+            // 꺼낸 노드와 연결된 모든 노드들만큼 반복
+            for (int connection : connections) {
+                // 해당 노드를 방문하지 않은 경우 큐에 추가
+                if (!visited[connection]) {
+                    q.add(connection);
+                    visited[connection] = true;
+                }
             }
         }
     }
     
-    // bfs는 큐를 이용한다.
-    private static void bfs(int n, int start) {
-        boolean[] visited = new boolean[n + 1];
-        Queue<Integer> q = new LinkedList<>();
-        
-        q.offer(start);
+    
+    /* DFS */
+    private static void dfs(int start) {
         visited[start] = true;
         
-        while (!q.isEmpty()) {
-            int v = q.poll();
-            System.out.print(v + " ");
-            
-            List<Integer> conn = graph.get(v);
-            Collections.sort(conn);     // 가장 낮은 수부터 방문
-            for (int c : conn) {
-                if (!visited[c]) {
-                    q.offer(c);
-                    visited[c] = true;
-                }
+        System.out.print(start + " ");
+        
+        // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+        List<Integer> connections = graph.get(start);
+        
+        for (int connection : connections) {
+            // 만약 해당 노드를 방문하지 않은 경우 재귀
+            if (!visited[connection]) {
+                dfs(connection);
             }
         }
     }
