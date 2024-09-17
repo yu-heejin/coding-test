e6662d662cfd7e7da264162ba1f1359380ed12d0
yu-heejin <yuheejin0914@gmail.com>
2024-09-17 22:23:47 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,14 +1,21 @@
+# 시간대 테이블
 WITH RECURSIVE time_table AS (
     SELECT 0 AS hour
-    UNION ALL
+    UNION
     SELECT hour + 1
     FROM time_table
     WHERE hour < 23
+),
+# 각 시간대별 입양 정보
+outs_per_time AS (
+    SELECT animal_id
+        , HOUR(datetime) AS out_time
+    FROM animal_outs
 )
 
 SELECT T.hour
-    , IFNULL(COUNT(A.datetime), 0) AS count
-FROM time_table T
-    LEFT JOIN animal_outs A ON T.hour = HOUR(A.datetime)
+    , COUNT(P.animal_id) AS count
+FROM outs_per_time P
+    RIGHT JOIN time_table T ON T.hour = P.out_time
 GROUP BY T.hour
-;
+ORDER BY T.hour;
