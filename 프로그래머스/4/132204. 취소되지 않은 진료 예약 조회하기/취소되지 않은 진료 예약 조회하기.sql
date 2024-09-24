3d7878818e9974556c128e92d70e011edb1a66a4
hoody <yuheejin0914@gmail.com>
2024-09-24 23:00:20 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -5,10 +5,9 @@ SELECT A.apnt_no
     , D.dr_name
     , A.apnt_ymd
 FROM appointment A
-    JOIN patient P ON P.pt_no = A.pt_no
-    JOIN doctor D ON D.dr_id = A.mddr_id
-WHERE A.mcdp_cd = 'CS'
+    JOIN patient P ON A.pt_no = P.pt_no
+    JOIN doctor D ON A.mddr_id = D.dr_id
+WHERE A.apnt_ymd LIKE '2022-04-13%'
     AND A.apnt_cncl_yn = 'N'
-    AND DATE_FORMAT( A.apnt_ymd, '%Y-%m-%d') = '2022-04-13'
 ORDER BY A.apnt_ymd
 ;
