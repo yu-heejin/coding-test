e2b0c81f6be61c541e33c36a4f30fbd2c0276388
hoody <yuheejin0914@gmail.com>
2024-10-28 22:50:14 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,58 +1,58 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     
-    static final int[] MOVE_X = {-1, 1, 0, 0};
-    static final int[] MOVE_Y = {0, 0, -1, 1};
-    static int n, m;
-    static int[][] board;
+    private static final int[] MOVE_X = {-1, 1, 0, 0};
+    private static final int[] MOVE_Y = {0, 0, -1, 1};
     
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
-        
-        board = new int[n][m];
-        
-        for (int i = 0; i < n; i++) {
-            input = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(input[j]);
+
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        int[][] board = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = line.charAt(j) - '0';
             }
         }
-        
-        bfs(0, 0);
+
+        System.out.println(bfs(board, N, M));
     }
-    
-    private static void bfs(int x, int y) {
+
+    private static int bfs(int[][] board, int N, int M) {
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        
-        q.offer(new int[] {x, y, 1});     // 현재 위치와 현재 위치일 때의 이동한 칸의 수
-        visited[x][y] = true;
-        
+        boolean[][] visited = new boolean[N][M];
+
+        q.add(new int[] {0, 0, 1});
+        visited[0][0] = true;
+
         while (!q.isEmpty()) {
             int[] curr = q.poll();
-            
-            if (curr[0] == n - 1 && curr[1] == m - 1) {
-                System.out.println(curr[2]);
-                break;
+
+            if (curr[0] == N - 1 && curr[1] == M - 1) {
+                return curr[2];
             }
-            
+
             for (int i = 0; i < 4; i++) {
                 int nx = curr[0] + MOVE_X[i];
                 int ny = curr[1] + MOVE_Y[i];
-                
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                
-                if (!visited[nx][ny] && board[nx][ny] == 1) {
-                    q.offer(new int[] {nx, ny, curr[2] + 1});
-                    visited[nx][ny] = true;
-                }
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (visited[nx][ny]) continue;
+                if (board[nx][ny] == 0) continue;
+
+                q.add(new int[] {nx, ny, curr[2] + 1});
+                visited[nx][ny] = true;
             }
         }
+
+        return -1;
     }
 }
