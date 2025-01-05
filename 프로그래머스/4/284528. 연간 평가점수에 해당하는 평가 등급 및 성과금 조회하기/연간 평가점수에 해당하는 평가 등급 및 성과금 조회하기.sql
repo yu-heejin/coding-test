151bba22427011572a282c21c4af6d61c247f5f1
yu-heejin <yuheejin0914@gmail.com>
2025-01-05 23:25:30 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,6 +1,6 @@
-# 각 사원별 평균 평가 점수
+# 각 직원의 평균 점수 및 등급
 WITH hr_grade_avg AS (
-    SELECT emp_no
+    SELECT emp_no 
         , AVG(score) AS avg_score
         , CASE WHEN AVG(score) >= 96 THEN 'S'
             WHEN AVG(score) >= 90 THEN 'A'
@@ -14,12 +14,11 @@ WITH hr_grade_avg AS (
 SELECT E.emp_no
     , E.emp_name
     , G.grade
-    , CASE G.grade WHEN 'S' THEN sal * (20 / 100) 
-        WHEN 'A' THEN sal * (15 / 100) 
-        WHEN 'B' THEN sal * (10 / 100)
+    , CASE WHEN G.grade = 'S' THEN E.sal * (20 / 100)
+        WHEN G.grade = 'A' THEN E.sal * (15 / 100)
+        WHEN G.grade = 'B' THEN E.sal * (10 / 100)
         ELSE 0
-     END AS bonus
-FROM hr_employees E
-    JOIN hr_grade_avg G ON E.emp_no = G.emp_no
-ORDER BY E.emp_no
-;
+    END AS bonus
+FROM hr_grade_avg G
+    JOIN hr_employees E ON G.emp_no = E.emp_no
+ORDER BY E.emp_no;
