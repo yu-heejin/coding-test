514832775247a980ee0b85b3d908b0823df9c34c
yu-heejin <yuheejin0914@gmail.com>
2025-01-13 23:14:22 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,7 @@
-SELECT O.animal_id
-    , O.name
+SELECT I.animal_id
+    , I.name
 FROM animal_ins I
     JOIN animal_outs O ON I.animal_id = O.animal_id
-WHERE I.datetime > O.datetime
-ORDER BY I.datetime;
+        AND I.datetime > O.datetime
+ORDER BY I.datetime
+;
