6b5be2175bf24b75d0cbdae1e7ad28f069c106b0
yu-heejin <yuheejin0914@gmail.com>
2025-02-11 00:21:21 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,18 +1,13 @@
-# 게시물 작성 횟수
-WITH board_count AS (
-    SELECT writer_id
-        , COUNT(*) AS board_cnt
-    FROM used_goods_board U
-    GROUP BY writer_id
-)
-
-# substr(문자열, 시작 위치, 가져올 길이)
 SELECT U.user_id
     , U.nickname
-    , CONCAT(U.city, ' ', U.street_address1, ' ', U.street_address2) AS '전체주소'
-    , CONCAT(SUBSTR(U.tlno, 1, 3), '-', SUBSTR(U.tlno, 4, 4), '-', SUBSTR(U.tlno, 8, 4)) AS '전화번호'
-FROM board_count B
-    JOIN used_goods_user U ON B.writer_id = U.user_id
-WHERE board_cnt >= 3
-ORDER BY U.user_id DESC
-;
+    , CONCAT(U.city, ' ', U.street_address1, ' ', U.street_address2) AS 전체주소
+    , CONCAT(SUBSTR(U.tlno, 1, 3), '-', SUBSTR(U.tlno, 4, 4), '-', SUBSTR(U.tlno, 8, 4)) AS 전화번호
+FROM (
+    SELECT writer_id
+        , COUNT(*) AS board_count
+    FROM used_goods_board
+    GROUP BY writer_id
+) T
+    JOIN used_goods_user U ON T.writer_id = U.user_id
+    AND T.board_count >= 3
+ORDER BY U.user_id DESC;
