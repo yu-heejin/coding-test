937aa6d2bd059b0f8b50f5f332f87cfa4e11e647
Ellie <yuheejin0914@gmail.com>
2025-05-23 00:20:24 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+}
