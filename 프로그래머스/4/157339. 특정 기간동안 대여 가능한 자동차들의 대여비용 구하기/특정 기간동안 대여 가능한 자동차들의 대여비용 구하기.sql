d5f93e71ad776a431d27c9a64892e1aa7a6a270c
Ellie <yuheejin0914@gmail.com>
2025-06-06 22:59:58 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,13 +1,19 @@
-# 자동차 종류가 세단, SUV인 자동차 중 11월에 대여 가능한 자동차
-# 11월에 대여 가능하려면 11월 1일 ~ 30일 사이에 기록이 없어야한다
-WITH sedan_suv_car AS (
+# 11월 1일 ~ 11월 30일까지 대여 가능한 세단, SUV
+# 대여 가능 조건: 대여 기록이 없음, 11/1 ~ 11/30까지 대여 기록 없음
+WITH available_car AS (
     SELECT DISTINCT C.car_id
         , C.car_type
         , C.daily_fee
-        , C.options
     FROM car_rental_company_car C
+        # 기록이 없어도 대여 가능하려면 LEFT JOIN
         LEFT JOIN car_rental_company_rental_history H ON C.car_id = H.car_id
-    WHERE (C.car_type = '세단' OR C.car_type = 'SUV')
+    WHERE C.car_type IN ('세단', 'SUV')
+        # AND C.car_id NOT IN (
+        #     SELECT car_id
+        #     FROM car_rental_company_rental_history
+        #     WHERE (start_date >= '2022-11-01' OR start_date <= '2022-11-30')
+        #         AND (end_date >= '2022-11-01' OR end_date <= '2022-11-30')
+        # )
         AND C.car_id NOT IN (
             SELECT car_id
             FROM car_rental_company_rental_history
@@ -15,21 +21,21 @@ WITH sedan_suv_car AS (
                 OR end_date >= '2022-11-01'
         )
 ),
-# 30일간의 대여 금액
-month_rental_fee AS (
-    SELECT C.car_id
-        , C.car_type
-        , ROUND(C.daily_fee * (1 - P.discount_rate / 100)) * 30 AS fee
-    FROM sedan_suv_car C
-        JOIN car_rental_company_discount_plan P ON C.car_type = P.car_type
-            AND P.duration_type = '30일 이상'
+# 30일간의 대여 금액 (할인 포함)
+total_rental_cost AS (
+SELECT A.car_id
+    , A.car_type
+    , ROUND(A.daily_fee * (1 - P.discount_rate / 100) * 30) AS fee
+FROM available_car A
+    JOIN car_rental_company_discount_plan P ON A.car_type = P.car_type
+    AND P.duration_type = '30일 이상'
 )
 
 SELECT *
-FROM month_rental_fee
+FROM total_rental_cost
 WHERE fee >= 500000
     AND fee < 2000000
 ORDER BY fee DESC
-    , C.car_type
-    , C.car_id DESC
+    , car_type
+    , car_id DESC
 ;
