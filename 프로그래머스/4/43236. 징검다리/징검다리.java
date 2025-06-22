2dcc8e78f1e1790104ef17e3e38db4a41e2468ae
Ellie <yuheejin0914@gmail.com>
2025-06-22 15:55:27 +0900

    [level 4] Title: 징검다리, Time: 39.43 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -33,27 +33,14 @@ class Solution {
                 // 지워야 할 돌보다 지운 돌이 더 많으면 mid(최소거리)를 줄여 지워지는 돌의 개수를 줄여야 한다.
                 right = mid - 1;
             } else if (count <= n) {
-                // 지워야 할 돌보다 지운 돌이 적으면 mid를 늘여 지워지는 돌의 개수를 늘려야한다.
+                // 지워야 할 돌보다 지운 돌이 적으면 mid를 늘려 지워지는 돌의 개수를 늘려야한다.
+                // 값이 같더라도, 최대값이 더 있을 수 있으므로 mid를 늘린다.
+                // if (count == n) answer = mid; 오류, n개가 아니라 n개 이하
                 answer = mid;
                 left = mid + 1;
             }
         }
         
         return answer;
-    }
-    
-    private int getDistance(int distance, int[] rocks, int index) {
-        int before = 0;
-        int after = distance;
-        
-        if (index > 0) {
-            before = rocks[index - 1];
-        }
-        
-        if (index < rocks.length - 1) {
-            after = rocks[index + 1];
-        }
-        
-        return Math.abs(after - before);
     }
 }
