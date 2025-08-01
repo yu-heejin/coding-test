de7b18a897f9514ff8cf1bdd8d172e54f571c0ec
Ellie <yuheejin0914@gmail.com>
2025-08-01 18:10:43 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,37 +2,47 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 하루 지날 때마다 speeds만큼 작업률 상승
-        // 앞에 있는 작업이 모두 끝나야 뒤에 있는 작업이 배포된다.
-        List<Integer> answer = new ArrayList<>();
-        Queue<int[]> q = new LinkedList<>();
+        List<Integer> tempAnswer = new ArrayList<>();
+        Queue<Integer> tasks = new LinkedList<>();
         
-        // 큐에 작업을 담는다.
+        // 배포까지 필요한 날짜를 큐에 삽입
         for (int i = 0; i < progresses.length; i++) {
-            q.add(new int[] { progresses[i], speeds[i] });
+            // 0으로 안 나누어 떨어지면 하루 더 일해야함
+            if ((100 - progresses[i]) % speeds[i] != 0) {
+                tasks.offer((100 - progresses[i]) / speeds[i] + 1);
+            } else {
+                tasks.offer((100 - progresses[i]) / speeds[i]);
+            }
         }
         
-        while (!q.isEmpty()) {
-            int count = 0;
-            
-            // speeds만큼 더해준다.
-            for (int[] p : q) {
-                p[0] += p[1];
-            }
-            
-            // 오늘 몇개의 기능이 배포되는지 확인
-            while (!q.isEmpty() && q.peek()[0] >= 100) {
+        // 첫번째 기능 배포 날짜
+        int deploy = tasks.peek();
+        int count = 0;
+        
+        while (!tasks.isEmpty()) {
+            // 현재 배포 날짜가 큐의 다음기능 배포 날짜보다 크면 제거
+            if (tasks.peek() <= deploy) {
                 count++;
-                q.poll();
+                tasks.poll();
+            } else {
+                // 더이상 제거할 수 없는 경우 정답 추가
+                tempAnswer.add(count);
+                count = 0;
+                deploy = tasks.peek();    // 다음 배포 날짜
             }
-            
-            if (count > 0) {
-                answer.add(count);
-            }
-            
-            count = 0;
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        // 마지막 배포
+        if (count > 0) {
+            tempAnswer.add(count);
+        }
+        
+        int[] answer = new int[tempAnswer.size()];
+        
+        for (int i = 0; i < tempAnswer.size(); i++) {
+            answer[i] = tempAnswer.get(i);
+        }
+        
+        return answer;
     }
 }
