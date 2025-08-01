d2e7cdc01de85d416026085f05d28708b59d9951
Ellie <yuheejin0914@gmail.com>
2025-08-01 16:49:17 +0900

    [level 3] Title: 베스트앨범, Time: 1.63 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,65 +1,86 @@
 import java.util.*;
 
+class Music implements Comparable<Music> {
+    int index;
+    int plays;
+    String genre;
+    
+    public Music(int index, int plays, String genre) {
+        this.index = index;
+        this.plays = plays;
+        this.genre = genre;
+    }
+    
+    @Override
+    public int compareTo(Music o) {
+        if (o.plays == this.plays) {
+            return this.index - o.index;
+        }
+        
+        return o.plays - this.plays;
+    }
+    
+    @Override
+    public String toString() {
+        return "[" + index + "] " + genre + ", " + plays;
+    }
+}
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        // 가장 많이 재생된 장르
-        HashMap<String, Integer> genreCount = new HashMap<>();
+        Map<String, List<Music>> playsPerGenres = new HashMap<>();
+        
+        // 장르, 노래, 고유 번호 저장
         for (int i = 0; i < genres.length; i++) {
-            genreCount.put(
-                genres[i], 
-                genreCount.getOrDefault(genres[i], 0) + plays[i]
-            );
-        }
-        // 장르 정렬
-        List<Map.Entry<String, Integer>> entries = new LinkedList<>(genreCount.entrySet());
-        List<String> genresSort = new ArrayList<>();
-        entries.sort((o1, o2) -> {
-            return genreCount.get(o2.getKey()) - genreCount.get(o1.getKey());
-        });
-        
-        // 정렬된 장르순으로 삽입
-        for (Map.Entry<String, Integer> entry : entries) {
-            genresSort.add(entry.getKey());
+            Music m = new Music(i, plays[i], genres[i]);
+            
+            List<Music> musics = playsPerGenres.getOrDefault(genres[i], new ArrayList<Music>());
+            musics.add(m);
+            playsPerGenres.put(genres[i], musics);
         }
         
-        // 장르별 가장 많이 재생된 노래
-        List<Integer> pq = new ArrayList<>();
-        
-        for (int i = 0; i < genres.length; i++) {
-            pq.add(i);
+        // 해당 장르 내에서 가장 많이 재생된 노래 확인
+        // 정렬
+        for (String key : playsPerGenres.keySet()) {
+            Collections.sort(playsPerGenres.get(key));
         }
         
-        Collections.sort(pq, (o1, o2) -> {
-            // 횟수가 같으면 고유번호가 가장 낮은 순서대로
-            if (plays[o2] == plays[o1]) {
-                return o1 - o2;
+        // 크기순으로 key 정렬 - 정렬을 위한 set to list
+        List<String> keys = new ArrayList<>(playsPerGenres.keySet());
+        
+        Collections.sort(keys, (o1, o2) -> {
+            List<Music> musics1 = playsPerGenres.get(o1);
+            List<Music> musics2 = playsPerGenres.get(o2);
+            int sum1 = 0;
+            int sum2 = 0;
+            
+            for (Music m : musics1) {
+                sum1 += m.plays;
             }
             
-            // 횟수가 큰 순서로 정렬
-            return plays[o2] - plays[o1];
+            for (Music m : musics2) {
+                sum2 += m.plays;
+            }
+            
+            return sum2 - sum1;
         });
         
-        // 장르별 가장 많이 재생된 노래 2개씩 선곡
-        List<Integer> result = new ArrayList<>();
-        boolean[] visited = new boolean[pq.size()];
-        for (String genre : genresSort) {
+        List<Integer> tempAnswer = new ArrayList<>();
+        
+        for (String key : keys) {
             int count = 0;
-            for (Integer i : pq) {
-                if (!visited[i] && genres[i].equals(genre)) {
-                    visited[i] = true;
-                    result.add(i);
-                    count++;
-                }
+            for (Music m : playsPerGenres.get(key)) {
+                if (count >= 2) break;
                 
-                if (count >= 2) {
-                    break;
-                }
+                tempAnswer.add(m.index);
+                count++;
             }
         }
         
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
+        int[] answer = new int[tempAnswer.size()];
+        
+        for (int i = 0; i < tempAnswer.size(); i++) {
+            answer[i] = tempAnswer.get(i);
         }
         
         return answer;
