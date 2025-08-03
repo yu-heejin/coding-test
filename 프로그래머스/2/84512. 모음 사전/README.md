505481efb36dbbd34c2cd80df104ecd5d30cd64b
Ellie <yuheejin0914@gmail.com>
2025-08-03 22:11:46 +0900

    [level 2] Title: 모음 사전, Time: 15.03 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
 
 ### 성능 요약
 
-메모리: 79.5 MB, 시간: 2.85 ms
+메모리: 82.9 MB, 시간: 15.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 18:25:35
+2025년 08월 03일 22:11:43
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,38 +1,29 @@
-import java.util.*;
-
 class Solution {
-    
-    private final char[] words = { 'A', 'E', 'I', 'O', 'U' };
-    private int[] bucket;
-    private List<String> answer = new ArrayList<>();
+    char[] alphabet = {'A', 'E', 'I', 'O', 'U'};
+    int answer = 0;
+    int index = 0;
     
     public int solution(String word) {
-        for (int i = 1; i <= 5; i++) {
-            bucket = new int[i];
-            combination(5, i);
-        }
-        Collections.sort(answer);
+        dfs(new StringBuilder(), word);
         
-        return answer.indexOf(word) + 1;
+        return answer;
     }
     
-    // 매번 전체 아이템중에서 뽑는다.
-    private void combination(int n, int r) {
-        if (r == 0) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bucket.length; i++) {
-                sb.append(words[bucket[i]]);
-            }
-            answer.add(sb.toString());
+    private void dfs(StringBuilder s, String word) {
+        if (s.length() > 5) return;
+        
+        if (s.length() > 0) index++;
+        
+
+        if (s.toString().equals(word)) {
+            answer = index;
             return;
         }
-
-        int lastIndex = bucket.length - r - 1;
-        int smallest = 0;   // 매번 전체에서 뽑는다.
-
-        for (int i = smallest; i < n; i++) {
-            bucket[lastIndex + 1] = i;
-            combination(n, r - 1);
+        
+        for (int i = 0; i < 5; i++) {
+            s.append(alphabet[i]);
+            dfs(s, word);
+            s.deleteCharAt(s.length() - 1);
         }
     }
 }
