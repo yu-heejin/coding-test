fa28558252026e5356a1dcab21841af5fe3fbd9d
Ellie <yuheejin0914@gmail.com>
2025-08-17 21:57:43 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,32 +1,27 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    
-    public static void main(String args[]) throws Exception {
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int t = Integer.parseInt(br.readLine());
-        
+
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
+
             int[] dp = new int[n + 1];
-            
-            if (n == 1) {
-                dp[1] = 1;
-            } else if (n == 2) {
-                dp[1] = 1;
-                dp[2] = 2;
-            } else {
-                dp[1] = 1;
-                dp[2] = 2;
-                dp[3] = 4;
-            }
-            
+
+            dp[1] = 1;
+            if (n >= 2) dp[2] = 2;
+            if (n >= 3) dp[3] = 4;
+
             for (int j = 4; j <= n; j++) {
                 dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
             }
-            
+
             System.out.println(dp[n]);
         }
     }
