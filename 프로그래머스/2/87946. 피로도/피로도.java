02e22e05817020342ff5463b42ab755d222318e7
Ellie <yuheejin0914@gmail.com>
2025-10-01 20:50:42 +0900

    [level 2] Title: 피로도, Time: 2.49 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,25 @@
 class Solution {
-    private int answer = 0;
+    int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        dfs(new boolean[dungeons.length], dungeons, k, 0);
+        visitedDungeons(new boolean[dungeons.length], dungeons, k, 0);
         
         return answer;
     }
     
-    // 이전 인덱스도 방문해야 하기 때문에 시작 인덱스를 주지 말고 반복문은 0부터 시작해야 한다.
-    private void dfs(boolean[] visited, int[][] dungeons, int power, int count) {
+    // 모든 던전을 방문해보는 경우의 수
+    private void visitedDungeons(boolean[] visited, int[][] dungeons, int k, int count) {
         for (int i = 0; i < dungeons.length; i++) {
-            // 아직 방문하지 않았고, 최소 필요 피로도가 현재 힘보다 작거나 같을 때
-            if (!visited[i] && dungeons[i][0] <= power) {
+            // 최소 피로도는 항상 소모 피로도보다 크다
+            if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(visited, dungeons, power - dungeons[i][1], count + 1);
+                k -= dungeons[i][1];
+                visitedDungeons(visited, dungeons, k, count + 1);
+                k += dungeons[i][1];
                 visited[i] = false;
             }
         }
         
-        // 던전 방문 순서는 첫번째 -> 세번째 -> 두번째 순으로도 방문 가능한 것으로 보아 모든 던전을 방문해야한다.
         answer = Math.max(answer, count);
     }
 }
